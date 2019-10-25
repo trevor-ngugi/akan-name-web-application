c405ff6bb1ab53a8fa586f19bb00e39a2ea2efc1
trevor ngugi <ngugitrevor@gmail.com>
2019-10-25 16:25:18 +0300

    create variables in js with name1 name 2

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//to get input from html and save it in variable
+
+var name1="firstname"
+var name2="lastname"
+var greet=name1 +lastname
+
+var nameValue = document.getElementById("uniqueID");
+alert(nameValue)
