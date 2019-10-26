1dcde0e86e34be03d5a8d90057281aed6e1316c8
trevor ngugi <ngugitrevor@gmail.com>
2019-10-26 13:32:25 +0300

    add function of cc in js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 
 var name1="firstname"
 var name2="lastname"
-var greet=name1 +lastname
 
-var nameValue = document.getElementById("uniqueID");
-alert(nameValue)
+alert(x)
+// calculates the function of cc
+var cc=[19,20,21]
+function century(){
+return (cc[0]/4)-2*cc[0]-1
+}
