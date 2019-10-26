bd05ebc6e0239ce32bbd0553dd7792e1c6792bf3
trevor ngugi <ngugitrevor@gmail.com>
2019-10-26 15:40:17 +0300

    add if control flow for choosing male name

--- js/script.js
+++ js/script.js
@@ -30,8 +30,18 @@ var dayOfMonth=[27,28,29]
 var dayOfWeek=(centuryResult+yearResult+monthResult+dayOfMonth[2])%7
 
 //use get value of id  to put into the functions
-
+//variables of gender 
 var gender=["male ","female" ];
 
 var maleDays=["sunday", "monday", "tuesday", "wednesday", "thrusday", "friday", "saturday"];
 var maleNames=["Kwasi", "Kwadwo", "Kwabena" ,"Kwaku", "Yaw","Kofi", "Kwame"];
+
+//if function for male names
+if(dayOfWeek===0){console.log(maleNames[0]);}
+	else if (dayOfWeek===1){console.log(maleNames[1]);}
+    else if (dayOfWeek===2){console.log(maleNames[2]);}
+    else if (dayOfWeek===3){console.log(maleNames[3]);}
+    else if (dayOfWeek===4){console.log(maleNames[4]);}
+	else if (dayOfWeek===5){console.log(maleNames[5]);}
+    else if (dayOfWeek===6){console.log(maleNames[6]);}
+    //add else function to close it
