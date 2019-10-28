b0af1a6925bf7df9dc40f551fe9ba00852e7fedf
trevor ngugi <ngugitrevor@gmail.com>
2019-10-28 12:32:14 +0300

    add amle id and female id

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-if(("day"<=o || "day">31)||("month"<=0||"month">12){
+if(("day"<=o || "day">31) && ("month"<=0||"month">12){
 
+    function result(day,month,century,year,gender){
+        var century1=(parseInt(century)/4)-2*parseInt(century)-1;
+        var year1=5*parseInt(year)/4;
+        var month1=(26*(parseInt(month)+1)/10)+parseInt(day);
+        var dayOfWeek1=Math.trunc((century1+year1+month1)%7);
+        alert(dayOfWeek1);                           }
+                                                    }
+        else {alert("wrong details")}                                            
 
-
-function result(day,month,century,year,gender){
-    var century1=(parseInt(century)/4)-2*parseInt(century)-1;
-    var year1=5*parseInt(year)/4;
-    var month1=(26*(parseInt(month)+1)/10)+parseInt(day);
-    var dayOfWeek1=Math.trunc((century1+year1+month1)%7);
-
-    alert(dayOfWeek1);
+    
     //edit this function
-    var gender1=gender ;
+    
     var maleNames=["Kwasi", "Kwadwo", "Kwabena" ,"Kwaku", "Yaw","Kofi", "Kwame"];
     var femaleNames=[ "Akosua","Adwoa","Abenaa","Akua","Yaa","Afua", "Ama"];
-
-    if(dayOfWeek1===0 &&gender[0]){console.log(maleNames[0]);}
-        else if (dayOfWeek1===1 &&gender[0]){console.log(maleNames[1]);}
-        else if (dayOfWeek1===2 &&gender[0]){console.log(maleNames[2]);}
-        else if (dayOfWeek1===3 &&gender[0]){console.log(maleNames[3]);}
-        else if (dayOfWeek1===4 &&gender[0]){console.log(maleNames[4]);}
-        else if (dayOfWeek1===5 &&gender[0]){console.log(maleNames[5]);}
-        else if (dayOfWeek1===6 &&gender[0]){console.log(maleNames[6]);}
+{
+    if(dayOfWeek1===0 &&"male"checked){alert(maleNames[0]);}
+        else if (dayOfWeek1===1 &&"male"checked){alert(maleNames[1]);}
+        else if (dayOfWeek1===2 &&"male"checked){alert(maleNames[2]);}
+        else if (dayOfWeek1===3 &&"male"checked){alert(maleNames[3]);}
+        else if (dayOfWeek1===4 &&"male"checked){alert(maleNames[4]);}
+        else if (dayOfWeek1===5 &&"male"checked){alert(maleNames[5]);}
+        else if (dayOfWeek1===6 &&"male"checked){alert(maleNames[6]);}
 
     if(dayOfWeek1===0 &&gender[1]){console.log(femaleNames[0]);}
         else if (dayOfWeek1===1 &&gender[1]){console.log(femaleNames[1]);}
@@ -30,11 +31,11 @@ function result(day,month,century,year,gender){
         else if (dayOfWeek1===5 &&gender[1] ){console.log(femaleNames[5]);}
         else if (dayOfWeek1===6 &&gender[1]){console.log(femaleNames[6]);}
 
+                                                }                             
 
-
-                                                }
-                                                    } 
-        else {alert("wrong details")}
+                                                
+                                                     
+    
 
 
  
